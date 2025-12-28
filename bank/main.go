2a3b6bf76316e38deeef931b9467c8c80ef60786
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2025-12-27 23:21:29 -0300

    environments and types

---
+++ bank/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var owner string = "Rodrigo"
+	var agencyNumber = 159
+	var accountNumber = 123456
+	var balance float64 = 1223.45
+
+	fmt.Println(owner, agencyNumber, accountNumber, balance)
+}
