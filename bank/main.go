bb1972e8ac73334d44288d98add1697d8992f2b5
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-01-05 00:06:14 -0300

    feat: transfer between accounts

--- bank/main.go
+++ bank/main.go
@@ -29,6 +29,17 @@ func (c *CurrentAccount) Deposit(amountDeposit float64) (string, float64) {
 	}
 }
 
+func (c *CurrentAccount) Transfer(amountTransfer float64, destinationAccount *CurrentAccount) string {
+	canTransfer := amountTransfer > 0 && amountTransfer <= c.balance
+	if canTransfer {
+		c.balance -= amountTransfer
+		destinationAccount.balance += amountTransfer
+		return "Transfer successful"
+	} else {
+		return "Insufficient balance"
+	}
+}
+
 func main() {
 	accountRodrigo := CurrentAccount{
 		owner:         "Rodrigo",
@@ -37,7 +48,15 @@ func main() {
 		balance:       1234.50,
 	}
 
+	accountAmanda := CurrentAccount{
+		owner:         "Amanda",
+		agencyNumber:  123,
+		accountNumber: 123456,
+		balance:       1234.50,
+	}
+
 	fmt.Println("Balance Rodrigo:", accountRodrigo.balance)
-	fmt.Println(accountRodrigo.Withdraw(100.00))
-	fmt.Println("Balance Rodrigo after withdrawal:", accountRodrigo.balance)
+	fmt.Println(accountRodrigo.Transfer(100.00, &accountAmanda))
+	fmt.Println("Balance Rodrigo after transfer:", accountRodrigo.balance)
+	fmt.Println("Balance Amanda after transfer:", accountAmanda.balance)
 }
