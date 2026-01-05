bdb10c6150565fb82080b9518ed5509c56a48c5e
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-01-04 23:42:19 -0300

    feat: WIthdraw

--- bank/main.go
+++ bank/main.go
@@ -9,19 +9,25 @@ type CurrentAccount struct {
 	balance       float64
 }
 
+func (c *CurrentAccount) Withdraw(amountWithdraw float64) string {
+	canWithdraw := amountWithdraw > 0 && amountWithdraw <= c.balance
+	if canWithdraw {
+		c.balance -= amountWithdraw
+		return "Withdrawal successful"
+	} else {
+		return "Insufficient balance"
+	}
+}
+
 func main() {
-	ownerRodrigo := CurrentAccount{
+	accountRodrigo := CurrentAccount{
 		owner:         "Rodrigo",
 		agencyNumber:  123,
 		accountNumber: 123456,
 		balance:       1234.50,
 	}
 
-	fmt.Println(ownerRodrigo)
-
-	var accountRodrigo *CurrentAccount
-	accountRodrigo = new(CurrentAccount)
-	accountRodrigo.owner = "Rodrigo"
-
-	fmt.Println(*accountRodrigo)
+	fmt.Println("Balance Rodrigo:", accountRodrigo.balance)
+	fmt.Println(accountRodrigo.Withdraw(100.00))
+	fmt.Println("Balance Rodrigo after withdrawal:", accountRodrigo.balance)
 }
