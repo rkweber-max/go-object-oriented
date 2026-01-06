6dc027adfdc1471782658be92c1e7f6fc1c3914b
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-01-06 12:22:02 -0300

    feat: implement paymentBoleto function for account withdrawals and update main function to reflect balance after payment

--- bank/main.go
+++ bank/main.go
@@ -6,8 +6,19 @@ import (
 	"github.com/rkweber-max/go-object-oriented/bank/accounts"
 )
 
+func paymentBoleto(account *accounts.CurrentAccount, amount float64) {
+	account.Withdraw(amount)
+}
+
+type verifyAccount interface {
+	Withdraw(amount float64) string
+}
+
 func main() {
 	accountRodrigo := accounts.CurrentAccount{}
+	accountRodrigo.Deposit(100.00)
 
-	fmt.Println(accountRodrigo)
+	paymentBoleto(&accountRodrigo, 100.00)
+
+	fmt.Println(accountRodrigo.GetBalance())
 }
